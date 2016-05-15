8168fd60d48afe181a4a673f9d4003d5a7dd52cd
Gusten <johan.gusten@gmail.com>
2016-05-15 13:36:42 +0200

    Updates android launcher icon and adds some early code for fetching match data

--- Client/index.android.js
+++ Client/index.android.js
@@ -5,16 +5,40 @@
 
 import React, { Component } from 'react';
 import {
+  Image,
   AppRegistry,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
+var steamApiKey = "6F39056ADA5041ED08F9480773890A58";
+
 class DotaCreeper extends Component {
+  componentDidMount() {
+	  this.interval = setInterval(refresh, 15000);
+  }	
+  
+  componentWillUnmount() {
+	  clearInterval(this.interval);
+  }
+  
+  refresh() {
+	fetch("https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v1/?key=" + steamApiKey)
+		.then((response) => response.json())
+		.then((responseData) => {
+			console.log(responseData.result.games)
+		})
+	.done();
+  }
+
   render() {
     return (
       <View style={styles.container}>
+		<Image
+		  style={styles.icon}
+		  source={require('./minimap.png')}
+		/>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
